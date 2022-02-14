70608f3a83005728b723b1bfa1103d255f2163c3
Hugo Desmeules <hugod@momentfactory.com>
2022-02-14 11:15:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# fileTreeW3
+
+fileTreeW3 is a NodeJS app for serving file tree directories on a browser with a file preview.
+The file preview supports common text and image filetypes.
+
+## Installation
+
+Clone or download repo. Then:
+
+```
+npm install
+```
+
+## Usage
+
+Launch app with arbitrary number of paths as arguments:
+```
+node fileTreeW3.js ~/Desktop/ ./an/index-dir/
+```
+Then open browser to:
+```
+http://localhost:3333/
+```
+
+## Contributing
+Comments and pull requests are welcome!
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ file-tree-w3.js
@@ -0,0 +1,66 @@
+const chokidar = require('chokidar')
+const fs = require('fs')
+const http = require('http')
+const WebSocketServer = require('websocket').server
+const express = require('express')
+const server = http.createServer()
+const port = 3333
+const wsport = 9898
+const app = express()
+
+app.use(express.static(__dirname))
+app.listen(port)
+
+console.log("Welcome to the fileTreeW3!")
+
+process.argv.splice(0, 2)
+
+function checkExist(item, index){
+	if (!fs.existsSync(item)) {
+		console.error('Directory ' + item + ' not found.!')
+		process.exit()
+	}
+}
+
+if (process.argv.length >= 1){
+	process.argv.forEach(checkExist)
+	console.log("You chose to view " + process.argv.length + " folder(s)")
+	console.log("Please open a browser to http://localhost:" + port)
+}else{
+	console.error("No folder was specified as argument.")
+	process.exit()
+}
+
+server.listen(wsport)
+const wsServer = new WebSocketServer({
+    httpServer: server
+})
+wsServer.on('request', function(request) {
+	var output = ""
+	var watcher = chokidar.watch(process.argv, {ignored: /^\./, persistent: true})
+	watcher
+	  .on('all', (event, path) => {
+	  console.log(event, path)
+	  sendEvent(event, path)
+	})
+    const connection = request.accept(null, request.origin)
+    connection.on('message', function(message) {
+        let file = message.utf8Data
+        if (fs.statSync(file).isFile()){
+            if (file.endsWith(".jpg") || file.endsWith(".gif") || file.endsWith(".png")){
+                connection.sendUTF("imgs" + "data:image/gif;base64,"+fs.readFileSync(file, 'base64'))
+            }else if (file.endsWith(".txt") || file.endsWith(".js") || file.endsWith(".json") || file.endsWith(".css")){
+                connection.sendUTF("data" + fs.readFileSync(file, 'utf8').toString().slice(0,1000))
+            }
+        }
+    })
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client has disconnected.')
+    })
+	function sendEvent(event,path){
+		if (connection.connected) {
+            path = path.replace(/\\/g, "/")
+			connection.sendUTF("tree" + JSON.stringify({event:event, path:path}))
+		}
+	}
+})
