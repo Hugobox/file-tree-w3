37597e52ac1c5bf4fe46cb9e2be99f8741a8295b
Hugobox <hugo@momentfactory.com>
2022-03-07 20:52:09 -0500

    adding image compression with sharp and code cleaning

--- file-tree-w3.js
+++ file-tree-w3.js
@@ -1,4 +1,5 @@
 const chokidar = require('chokidar')
+const sharp = require('sharp')
 const fs = require('fs')
 const http = require('http')
 const WebSocketServer = require('websocket').server
@@ -9,6 +10,8 @@ const wsport = 9898
 const app = express()
 
 app.use(express.static(__dirname))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/jstree', express.static(__dirname + '/node_modules/jstree/dist/'));
 app.listen(port)
 
 console.log("Welcome to the fileTreeW3!")
@@ -18,7 +21,7 @@ process.argv.splice(0, 2)
 function checkExist(item, index){
 	if (!fs.existsSync(item)) {
 		console.error('Directory ' + item + ' not found.!')
-		process.exit()
+		process.exit(9)
 	}
 }
 
@@ -28,33 +31,37 @@ if (process.argv.length >= 1){
 	console.log("Please open a browser to http://localhost:" + port)
 }else{
 	console.error("No folder was specified as argument.")
-	process.exit()
+	process.exit(9)
 }
 
-let watchedFiles = []
+let watched = []
 let watcher = chokidar.watch(process.argv, {ignored: /^\./, persistent: true})
 watcher
   .on('all', (event, path) => {
-	  let element = {}
-	  element.event = event
-	  element.path = path
-	watchedFiles.push(element)
+	let element = {event: event, path: path}
+	watched.push(element)
 })
 
 server.listen(wsport)
 const wsServer = new WebSocketServer({
     httpServer: server
 })
+
 wsServer.on('request', function(request) {
     const connection = request.accept(null, request.origin)
-	watchedFiles.forEach(sendEvent);
+	watched.forEach(sendEvent);
 	
     connection.on('message', function(message) {
         let file = message.utf8Data
         if (fs.statSync(file).isFile()){
             if (file.endsWith(".jpg") || file.endsWith(".gif") || file.endsWith(".png")){
-                connection.sendUTF("imgs" + "data:image/gif;base64,"+fs.readFileSync(file, 'base64'))
-            }else if (file.endsWith(".txt") || file.endsWith(".js") || file.endsWith(".json") || file.endsWith(".css")){
+				sharp(file)
+					.resize(400)
+					.jpeg({ mozjpeg: true })
+					.toBuffer()
+					.then( data => { connection.sendUTF("imgs" + "data:image/gif;base64," + data.toString('base64') )})
+					.catch( err => { console.error(err)});
+            }else if (file.endsWith(".txt") || file.endsWith(".js") || file.endsWith(".json") || file.endsWith(".css") || file.endsWith(".html")){
                 connection.sendUTF("data" + fs.readFileSync(file, 'utf8').toString().slice(0,1000))
             }
         }
